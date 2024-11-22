1a73466e446bc80f53f19d4538dae4976f53ebbc
HumptyDumpy01 <tuznikolas@gmail.com>
2024-11-22 16:19:49 +0200

    Added validation to service booking forms by using zod

--- components/Form/FillInForm.tsx
+++ components/Form/FillInForm.tsx
@@ -14,6 +14,8 @@ import Select from '@/components/UI/Select';
 import Input from '@/components/UI/Input';
 import ArrowIcon from '@/components/UI/ArrowIcon';
 import HighlightText from '@/components/Typography/HighlightText';
+import { serviceBookingSchema } from '@/utils/schemas';
+import scrollTo from '@/utils/functions/scrollTo';
 
 interface FillInFormType {
   mode: `photography` | `videography` | `success` | `error`;
@@ -22,6 +24,8 @@ interface FillInFormType {
 }
 
 function FillInForm({ mode, onClose }: FillInFormType) {
+  const [errors, setErrors] = useState<string>(``);
+
   const [chosenSelectionOptionsService, setChosenSelectionOptionsService] =
     useState(mode === `photography` ? photographyOptions : videographyOptions);
   const [chosenSelectionOptionsPackage, setChosenSelectionOptionsPackage] =
@@ -53,7 +57,24 @@ function FillInForm({ mode, onClose }: FillInFormType) {
     e.preventDefault();
     const currObject = e.currentTarget;
     const formData = new FormData(currObject);
-    const results = Object.fromEntries(formData.entries());
+    const results = Object.fromEntries(formData.entries()) as {
+      serviceChosen: string;
+      name: string;
+      email: string;
+      phone: string;
+      preferredWayOfCommunication: string;
+      package: string;
+    };
+
+
+    const validation = serviceBookingSchema.safeParse(results);
+
+    if (!validation.success) {
+      setErrors(validation.error.errors[0].message);
+      scrollTo(`form-heading`);
+      return;
+    }
+
     // resetting the form
     // currObject.reset();
     // output
@@ -61,108 +82,116 @@ function FillInForm({ mode, onClose }: FillInFormType) {
   }
 
   return (
-    <div className={`mt-11 sm:px-8 px-2 relative`}>
-      <div onClick={() => onClose(false)} className={`absolute -top-7 right-0 cursor-pointer
+    <>
+      <div className={`mt-11 sm:px-8 px-2 relative`}>
+        <div onClick={() => onClose(false)} className={`absolute -top-7 right-0 cursor-pointer
       transition-all duration-200 hover:rotate-180 hover:scale-110`}>
-        <svg className={`w-20 lg:w-28 h-20 lg:h-28`} xmlns="http://www.w3.org/2000/svg" width="120" height="120"
-             viewBox="0 0 125 125"
-             fill="none">
-          <path
-            d="M62.5 7.8125C32.0312 7.8125 7.8125 32.0312 7.8125 62.5C7.8125 92.9688 32.0312 117.188 62.5 117.188C92.9688 117.188 117.188 92.9688 117.188 62.5C117.188 32.0312 92.9688 7.8125 62.5 7.8125ZM83.5938 89.8438L62.5 68.75L41.4062 89.8438L35.1562 83.5938L56.25 62.5L35.1562 41.4062L41.4062 35.1562L62.5 56.25L83.5938 35.1562L89.8438 41.4062L68.75 62.5L89.8438 83.5938L83.5938 89.8438Z"
-            fill="#DFDFDF" />
-        </svg>
-      </div>
-      <Heading animation={false} heading={(
-        <div className={`font-raleway font-semibold sm:text-7xl text-5xl`}>
-          Let’s fill in the form!
+          <svg className={`w-20 lg:w-28 h-20 lg:h-28`} xmlns="http://www.w3.org/2000/svg" width="120" height="120"
+               viewBox="0 0 125 125"
+               fill="none">
+            <path
+              d="M62.5 7.8125C32.0312 7.8125 7.8125 32.0312 7.8125 62.5C7.8125 92.9688 32.0312 117.188 62.5 117.188C92.9688 117.188 117.188 92.9688 117.188 62.5C117.188 32.0312 92.9688 7.8125 62.5 7.8125ZM83.5938 89.8438L62.5 68.75L41.4062 89.8438L35.1562 83.5938L56.25 62.5L35.1562 41.4062L41.4062 35.1562L62.5 56.25L83.5938 35.1562L89.8438 41.4062L68.75 62.5L89.8438 83.5938L83.5938 89.8438Z"
+              fill="#DFDFDF" />
+          </svg>
         </div>
-      )} />
-      <form onSubmit={handleSubmit} className={`flex flex-col bp-938:grid md:rid-cols-2 mt-[60px] gap-10`}>
-        <div className={`flex flex-col gap-8`}>
-          <Select
-            label={`service chosen`}
-            name={`serviceChosen`}
-            options={chosenSelectionOptionsService}
-            onChange={handleServiceChange}
-          />
+        <Heading animation={false} heading={(
+          <div className={`font-raleway font-semibold sm:text-7xl text-5xl form-heading`}>
+            Let’s fill in the form!
+          </div>
+        )} />
 
-          <Input label={`your name`} name={`name`} placeholder={`e.g. Jane Doe`} />
-          <Input label={`your email`} name={`email`} placeholder={`e.g. jane.doe@gmil.com`} />
-        </div>
+        {errors && (
+          <div className={`text-red-500 p-4 mt-5 rounded-md mb-4 text-xl`}>
+            <p>{errors}</p>
+          </div>
+        )}
+        <form onSubmit={handleSubmit} className={`flex flex-col bp-938:grid md:rid-cols-2 mt-[60px] gap-10`}>
+          <div className={`flex flex-col gap-8`}>
+            <Select
+              label={`service chosen`}
+              name={`serviceChosen`}
+              options={chosenSelectionOptionsService}
+              onChange={handleServiceChange}
+            />
 
-        <div className={`flex flex-col gap-8 mb-14`}>
-          <Input label={`your phone`} name={`phone`} placeholder={`Your Phone`} />
-          <Select label={`preferred way of communication`} name={`preferredWayOfCommunication`}
-                  options={[
-                    {
-                      value: `email`,
-                      label: `Via Email`
-                    },
-                    {
-                      value: `phone`,
-                      label: `Via Phone`
-                    },
-                    { value: `socialMedia`, label: `Via Social Media` }]} />
-          <Select label={`package chosen`} name={`package`}
-                  options={chosenSelectionOptionsPackage} />
-        </div>
-        <div className={`text-left col-span-2`}>
-          <p className={`text-sm text-zinc-300 max-w-screen-lg leading-relaxed`}>After paying the deposit, check
-            your
-            email for a confirmation letter. You also
-            need
-            to fill in the <a target={`_blank`} href="" className={`underline underline-offset-2`}><HighlightText
-              text={`Google
+            <Input label={`your name`} name={`name`} placeholder={`e.g. Jane Doe`} />
+            <Input label={`your email`} name={`email`} placeholder={`e.g. jane.doe@gmil.com`} />
+          </div>
+
+          <div className={`flex flex-col gap-8 mb-14`}>
+            <Input label={`your phone`} name={`phone`} placeholder={`Your Phone`} />
+            <Select label={`preferred way of communication`} name={`preferredWayOfCommunication`}
+                    options={[
+                      {
+                        value: `email`,
+                        label: `Via Email`
+                      },
+                      {
+                        value: `phone`,
+                        label: `Via Phone`
+                      },
+                      { value: `socialMedia`, label: `Via Social Media` }]} />
+            <Select label={`package chosen`} name={`package`}
+                    options={chosenSelectionOptionsPackage} />
+          </div>
+          <div className={`text-left col-span-2`}>
+            <p className={`text-sm text-zinc-300 max-w-screen-lg leading-relaxed`}>After paying the deposit, check
+              your
+              email for a confirmation letter. You also
+              need
+              to fill in the <a target={`_blank`} href="" className={`underline underline-offset-2`}><HighlightText
+                text={`Google
             form`} /></a> after successfully paying the deposit. The link will be attached in the letter and provided
-            hereafter,
-            following a successful payment.</p>
+              hereafter,
+              following a successful payment.</p>
 
-          <div className={`flex lg:justify-between lg:items-center mt-10 mb-4 flex-col lg:flex-row gap-8 lg:gap-10`}>
-            <button
-              className={`font-federo uppercase justify-center text-yellow-500 px-16 py-7 border border-amber-500 sm:text-3xl text-xl text-nowrap flex lg:w-fit items-center gap-4 transition-all duration-200 active:text-white active:border-zinc-50
+            <div className={`flex lg:justify-between lg:items-center mt-10 mb-4 flex-col lg:flex-row gap-8 lg:gap-10`}>
+              <button
+                className={`font-federo uppercase justify-center text-yellow-500 px-16 py-7 border border-amber-500 sm:text-3xl text-xl text-nowrap flex lg:w-fit items-center gap-4 transition-all duration-200 active:text-white active:border-zinc-50
               `}>
-              Pay 100$ CAD Deposit
-              <div className={`hidden sm:flex`}>
-                <ArrowIcon color={`yellow`} style={`large`} />
-              </div>
-            </button>
-            <button
-              onClick={() => onClose(false)}
-              type={`button`}
-              className={`sm:text-3xl text-2xl bg-zinc-900 py-4 px-8 rounded-md text-zinc-500 hover:bg-zinc-950 transition-colors duration-200`}>
-              Cancel
-            </button>
-          </div>
-          <div>
-            <Tooltip
-              title={(
-                <ul>
-                  <li className={`text-[13px]`}>1. If the shoot takes place as scheduled, the deposit will be
-                    deducted from the
-                    total service cost.
-                  </li>
-                  <li className={`text-[13px]`}>2. If the shoot is canceled less than 72 hours before the scheduled
-                    time,
-                    the
-                    $100 CAD deposit will be
-                    non-refundable.
-                  </li>
-                </ul>
-              )}
-              placement={`top`}>
-              <p className={`text-[13px] text-zinc-300 max-w-screen-lg leading-relaxed mt-4`}>By clicking the &#34;Pay
-                100$
-                CAD
-                Deposit&#34; button, you agree to the <span
-                  className={`underline underline-offset-2 cursor-pointer`}><HighlightText
-                  text={`Terms and Conditions.`} />
+                Pay 100$ CAD Deposit
+                <div className={`hidden sm:flex`}>
+                  <ArrowIcon color={`yellow`} style={`large`} />
+                </div>
+              </button>
+              <button
+                onClick={() => onClose(false)}
+                type={`button`}
+                className={`sm:text-3xl text-2xl bg-zinc-900 py-4 px-8 rounded-md text-zinc-500 hover:bg-zinc-950 transition-colors duration-200`}>
+                Cancel
+              </button>
+            </div>
+            <div>
+              <Tooltip
+                title={(
+                  <ul>
+                    <li className={`text-[13px]`}>1. If the shoot takes place as scheduled, the deposit will be
+                      deducted from the
+                      total service cost.
+                    </li>
+                    <li className={`text-[13px]`}>2. If the shoot is canceled less than 72 hours before the scheduled
+                      time,
+                      the
+                      $100 CAD deposit will be
+                      non-refundable.
+                    </li>
+                  </ul>
+                )}
+                placement={`top`}>
+                <p className={`text-[13px] text-zinc-300 max-w-screen-lg leading-relaxed mt-4`}>By clicking the &#34;Pay
+                  100$
+                  CAD
+                  Deposit&#34; button, you agree to the <span
+                    className={`underline underline-offset-2 cursor-pointer`}><HighlightText
+                    text={`Terms and Conditions.`} />
                 </span>
-              </p>
-            </Tooltip>
+                </p>
+              </Tooltip>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+    </>
   );
 }
 
--- utils/schemas.ts
+++ utils/schemas.ts
@@ -10,3 +10,12 @@ export const sendingEmailDetailsSchema = z.object({
   subject: z.string().min(1, { message: `Subject is required` }).max(255),
   html: z.string().min(1, { message: `Content is required` })
 });
+
+export const serviceBookingSchema = z.object({
+  serviceChosen: z.enum([`photography`, `videography`, `both`]),
+  name: z.string().min(1, { message: `Name is required` }).max(255),
+  email: z.string().email({ message: `Invalid email address` }),
+  phone: z.string().min(10, { message: `Please provide a valid phone number!` }).max(15),
+  preferredWayOfCommunication: z.enum([`email`, `phone`, `socialMedia`]),
+  package: z.enum([`photoSession`, `videoShoot`, `both`, `none`])
+});
