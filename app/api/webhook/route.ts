20ee910c66df71283242bb9d04f3d8c8aaad220e
HumptyDumpy01 <tuznikolas@gmail.com>
2024-11-23 11:46:59 +0200

    Session object only contains 4 params. I do parse them only in webhook and try to inject them to send to owner and user.

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -26,8 +26,7 @@ export async function POST(req: NextRequest) {
     event = stripe.webhooks.constructEvent(buf, sig, webhookSecret);
   } catch (err) {
     console.error('Webhook signature verification failed.', err);
-    // @ts-ignore
-    return NextResponse.json({ error: `Webhook Error: ${err.message}` }, { status: 400 });
+    return NextResponse.json({ error: `Webhook Error: ${(err as Error).message}` }, { status: 400 });
   }
 
   if (event.type === 'checkout.session.completed') {
@@ -40,54 +39,53 @@ export async function POST(req: NextRequest) {
         email,
         name,
         phone,
-        serviceChosen,
-        preferredWayOfCommunication,
-        package: servicePackage
+        // serviceChosen,
+        preferredWayOfCommunication
+        // package: servicePackage
       } = session.metadata;
 
-      console.log('Metadata:', { email, name, phone, serviceChosen, preferredWayOfCommunication, servicePackage });
+      // console.log('Metadata:', { email, name, phone, serviceChosen, preferredWayOfCommunication, servicePackage });
 
       try {
         // Send emails
         await axios.post('/api/resend', {
-          subject: `New Booking is made at TimelessView! ${serviceChosen === `both` ? `Both Videography and Photography` : serviceChosen} Service`,
+          subject: `New Booking is made at TimelessView! Service`,
           html: `
-          <b>Please go to your Stripe Account and ensure that the payment from ${email} was actually successful!</b>
-          <br>
-          <p><strong>Service Chosen:</strong> ${serviceChosen}</p>
-          <p><strong>Name:</strong> ${name};</p>
-          <p><strong>Email:</strong> ${email};</p>
-          <p><strong>Phone:</strong> ${phone};</p>
-          <p><strong>Preferred Way of Communication:</strong> ${preferredWayOfCommunication};</p>
-          <p><strong>Package:</strong> ${servicePackage};</p>
-        `
+            <b>Please go to your Stripe Account and ensure that the payment from ${email} was actually successful!</b>
+            <br>
+<!--            <p><strong>Service Chosen:</strong> serviceChosen</p>-->
+            <p><strong>Name:</strong> ${name};</p>
+            <p><strong>Email:</strong> ${email};</p>
+            <p><strong>Phone:</strong> ${phone};</p>
+            <p><strong>Preferred Way of Communication:</strong> ${preferredWayOfCommunication};</p>
+<!--            <p><strong>Package:</strong> servicePackage</p>-->
+          `
         });
 
         await axios.post('/api/resend-to-user', {
           email,
           subject: `Your Booking at TimelessView!`,
           html: `
-          <h1>Dear ${name},</h1>
-          <p>Thank you for booking my ${serviceChosen === `both` ? `Videography and Photography` : serviceChosen} service. I have received your deposit payment and will contact you shortly with further details.</p>
-          <p>Right now, please feel free to fulfill this form, which would help me understand your needs better: <a href="">LINK</a></p>
-          <h2>Booking Details:</h2>
-          <p>Service Chosen: ${serviceChosen}</p>
-          <p>Name: ${name};</p>
-          <p>Email: ${email};</p>
-          <p>Phone: ${phone};</p>
-          <p>Preferred Way of Communication: ${preferredWayOfCommunication};</p>
-          <p>Package: ${servicePackage};</p>
-          <br>
-          <p>Best regards,</p>
-          <p>TimelessView, Olena Vinytska</p>
-        `
+            <h1>Dear ${name},</h1>
+            <p>Thank you for booking my services. I have received your deposit payment and will contact you shortly with further details.</p>
+            <p>Right now, please feel free to fulfill this form, which would help me understand your needs better: <a href="">LINK</a></p>
+            <h2>Booking Details:</h2>
+<!--            <p>Service Chosen: {serviceChosen}</p>-->
+            <p>Name: ${name};</p>
+            <p>Email: ${email};</p>
+            <p>Phone: ${phone};</p>
+            <p>Preferred Way of Communication: ${preferredWayOfCommunication};</p>
+<!--            <p>Package: {servicePackage};</p>-->
+            <br>
+            <p>Best regards,</p>
+            <p>TimelessView, Olena Vinytska</p>
+          `
         });
 
         console.log('Emails sent successfully');
       } catch (err) {
         console.error('Error handling checkout session completed event:', err);
-        // @ts-ignore
-        return NextResponse.json({ error: `Webhook Error: ${err.message}` }, { status: 500 });
+        return NextResponse.json({ error: `Webhook Error: ${(err as Error).message}` }, { status: 500 });
       }
     } else {
       console.error('No metadata found in session.');
