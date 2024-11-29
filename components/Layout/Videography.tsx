811465470724034b1b133f867d930f799daa1210
HumptyDumpy01 <tuznikolas@gmail.com>
2024-11-29 18:39:03 +0200

    Implemented a different mechanics for video slider.

--- components/Layout/Videography.tsx
+++ components/Layout/Videography.tsx
@@ -1,4 +1,7 @@
 import { motion } from 'framer-motion';
+import { videos } from '@/utils/variables';
+// @ts-ignore
+import Slider from 'react-slick';
 
 const videoVariants = {
   hidden: { opacity: 0, y: 100 },
@@ -13,41 +16,68 @@ const videoVariants = {
 };
 
 function Videography() {
-  const videos = [
-    'https://www.youtube.com/embed/YWU8Dbe_chU?si=w5ZFTVuD7-fq3Men',
-    'https://www.youtube.com/embed/GlmAH_opZos?si=XTMqtCMjAqvWITP-',
-    'https://www.youtube.com/embed/1dLeIdLddE8?si=D6dCxgISezbuFa4t',
-    'https://www.youtube.com/embed/B76GfvpW-L0?si=21arfQD1P6ys9FFa',
-    'https://www.youtube.com/embed/pc1LaQ-1bMA?si=XUWUb0Q8Ujsh_h_-',
-    'https://www.youtube.com/embed/ffligM1ocpM?si=B6YSZ1mYbVRY0VwL',
-    'https://www.youtube.com/embed/gCnxdmN0RoY'
-  ];
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
 
   return (
-    <div className={`flex overflow-x-auto overflow-y-hidden scrollbar-hide gap-8 items-center py-7`}>
-      {videos.map((video, index) => (
-        <motion.div
-          key={video}
-          custom={index}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: true }}
-          variants={videoVariants}
-          className={`flex`}
-        >
-          <iframe
-            width="400"
-            height="620"
-            src={video}
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className={`mr-5`}
-          ></iframe>
-        </motion.div>
-      ))}
-    </div>
+    <>
+      <div className={`overflow-x-auto overflow-y-hidden scrollbar-hide gap-8 items-center py-7 hidden sm:flex`}>
+        {videos.map((video, index) => (
+          <motion.div
+            key={video}
+            custom={index}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            variants={videoVariants}
+            className={`flex`}
+          >
+            <iframe
+              width="400"
+              height="620"
+              src={video}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              className={`mr-5`}
+            ></iframe>
+          </motion.div>
+        ))}
+      </div>
+
+      <div className={`sm:hidden`}>
+        <Slider {...settings}>
+          {videos.map((video, index) => (
+            <motion.div
+              key={video}
+              custom={index}
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true }}
+              variants={videoVariants}
+              className={`flex`}
+            >
+              <iframe
+                width="290"
+                height="520"
+                src={video}
+                title="YouTube video player"
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                className={`ml-5`}
+              ></iframe>
+            </motion.div>
+          ))}
+        </Slider>
+      </div>
+    </>
   );
 }
 
